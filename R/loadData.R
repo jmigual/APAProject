638ed218e055949a77b9206064b3718c0a00396f
jmigual <j.marce.igual@gmail.com>
2016-11-05 21:52:09 +0100

    Added R code to load data

---
+++ R/loadData.R
@@ -0,0 +1,33 @@
+rm(list=ls())
+data <- read.csv(file.choose(), sep=" ")
+
+cols = (ncol(data) - 1)/3
+
+Xcols = (1:cols)*3 - 1
+Ycols = 1 + Xcols
+Zcols = 1 + Ycols
+
+Tpoints = data[1]
+Xpoints = data[Xcols]
+Ypoints = -1 * data[Ycols]
+Zpoints = data[Zcols]
+
+Xcoord = stack(Xpoints)
+Ycoord = stack(Ypoints)
+
+pData = Xcoord
+pData$Yval = Ycoord$values
+pData$time = rep(1:nrow(data), ncol(Xpoints))
+pData = pData[,c(4,2,1,3)]
+names(pData)[2:3] = c("id", "XVal")
+
+library(googleVis)
+chartData <- gvisMotionChart(pData, idvar="id", timevar="time")
+h1 = chartData$html
+htmlstring <- paste(h1, collapse = "\n")
+cat(htmlstring, file="index.html")
+#cat(chartData$html, file="index.html")
+#cat(chartData$html$chart, file="char.html")
+
+
+
