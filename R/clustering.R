ff14c73e586dd12a2228950142c0b8743e8a78c2
jmigual <j.marce.igual@gmail.com>
2016-12-09 12:08:25 +0100

    Improved clustering code

--- R/clustering.R
+++ R/clustering.R
@@ -1,11 +1,21 @@
 rm(list=ls())
 library(MASS)
 
-data = read.csv(file.choose(), sep=" ", header = TRUE)
-data$target = read.csv(file.choose(), sep=" ", header = FALSE)
+plotPosNeg <- function(posX, negX, posY, negY, ...) {
+  X = c(posX, negX)
+  Y = c(posY, negY)
+  meanX = mean(X)
+  meanY = mean(Y)
+  plot(posX - meanX, posY - meanY, col='green', ylim=c(min(Y),max(Y)), xlim=c(min(X),max(X)), ...)
+  points(negX - meanX, negY - meanY, col='red')
+}
+
+data = read.csv(choose.files(caption="Selecciona dades", multi=FALSE), sep=" ", header = TRUE)
+data$target = read.csv(choose.files(caption="Selecciona resposta", multi=FALSE), sep=" ", header = FALSE)
 
 # Punt a ser projectat
 point = 60
+
 columns = 2 + (point*3):(point*3 + 2)
 pData = data[columns]
 
@@ -18,6 +28,9 @@ negative = subset(pData, target == 0)
 myLDA = lda(pData[1:3], unlist(data$target))
 
 # Imprimir resultats LDA
+plotPosNeg(positive[,1], negative[,1], positive[,2], negative[,2], 
+           main="Direction for projection using FDA", xlab="X", ylab="Y")
+abline(0, myLDA$scaling[2]/myLDA$scaling[1], col='black',lwd=2)
 plot(c(positive[,1],negative[,1]) - mean(pData[,1]), c(positive[,2],negative[,2]) - mean(pData[,2]), 
      col=c(rep('green',nrow(positive)),rep('red',nrow(negative))),
      main="Direction for projection using FDA", xlab="X", ylab="Y")
