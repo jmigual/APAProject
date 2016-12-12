8feb0049b3bfb1c5ae010cfc5fa81449696ee4c8
jmigual <j.marce.igual@gmail.com>
2016-12-12 17:06:18 +0100

    Fix some errors on read method

--- R/logistic.R
+++ R/logistic.R
@@ -1,27 +1,10 @@
 # Elmininar totes les dades anteriors
 rm(list=ls())
 library(MASS)
+source("./R/readAllData.R")
 
 # Aquest script realitza regressio logistica amb les dades obtingudes per a una sola expressio facial amb dos conjunts de dades,
 # Per entrenar es pot utilitzar la persona A i per provar les dades, la persona B
-
-readData = function(name){
-  filePath = choose.files(caption = paste("Selecciona dades", name), multi=FALSE)
-  fileName = basename(filePath)
-  folderPath = sub(fileName, "", filePath)
-  
-  dataName = sub("_targets", "_datapoints", fileName)
-  targetName = sub("_datapoints", "_targets", fileName)
-  
-  data = read.csv(paste0(folderPath, dataName), sep=" ", header = TRUE)
-  targetData = read.csv(paste0(folderPath, targetName), sep=" ", header = FALSE)
-  
-  targetFactors = as.factor(unlist(targetData[[1]]))
-  data$target = targetFactors
-  
-  # Remove the time column
-  return(data[, !names(data) %in% c("X0.0")])
-}
 
 checkValue = function(x) {
   if (x > 0) return(1)
@@ -29,7 +12,7 @@ checkValue = function(x) {
 }
 
 checkError = function(predicted, real, type) {
-  tab = table(predicted, real)
+  tab = table(factor(predicted, levels=0:1), real)
   print(paste("Error", type))
   print(tab)
   
@@ -41,25 +24,21 @@ trainAndError = function(data.train, data.test, family = quasibinomial, maxit = 
   # La columna target es la que es vol obtenir per regressio logistica
   glm.res = glm(target ~ ., data.train, family = family, control = list(maxit = maxit))
   
-  prob.train = exp(predict(glm.res, data.train))
-  val.train = as.factor(sapply(prob.train, checkValue))
-  
   # Obtenir el % d'error amb el conjunt de training
-  checkError(val.train, data[,301], "training")
-  
-  prob.test = exp(predict(glm.res, data.test))
-  val.test = as.factor(sapply(prob.test, checkValue))
+  prob.train = exp(predict(glm.res, newdata = data.train))
+  checkError(prob.train, data.train[,301], "training")
   
   # Obtenir el % d'error amb el conjunt de testing
-  checkError(val.test, dataT[,301], "testing")
+  prob.test = exp(predict(glm.res, newdata = data.test)) 
+  checkError(prob.test, data.test[,301], "testing")
 }
 
 # Llegir les dades training
-data = readData("training")
+data.train = readAllData("a", single = TRUE)
 
 # Llegir dades testing
-dataT = readData("testing")
+data.test = readAllData("b", single = TRUE)
 
 
 # Regressio utilitzant un model binomial
-trainAndError(data, dataT, "quasibinomial")
+trainAndError(data.train, data.test, "quasibinomial")
--- R/readAllData.R
+++ R/readAllData.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 
-readData = function(filePath){
+readData = function(filePath, single = TRUE){
   print(filePath)
   fileName = basename(filePath)
   folderPath = sub(fileName, "", filePath)
@@ -8,36 +8,30 @@ readData = function(filePath){
   dataName = sub("_targets", "_datapoints", fileName)
   targetName = sub("_datapoints", "_targets", fileName)
   
-  data = read.csv(paste0(folderPath, dataName), sep=" ", header = TRUE)
-  targetData = read.csv(paste0(folderPath, targetName), sep=" ", header = FALSE)
-  
-  expression = sub("^[ab]_", "", sub("_datapoints.txt", "", dataName))
-  data$target = lapply(unlist(targetData), function(x) {
-    if (x > 0) return(expression)
-    else return(as.character(x))
-  })
+  data = read.csv(paste0(folderPath, dataName), sep=" ", header = TRUE, colClasses = "numeric")
+  data$target = unlist(read.csv(paste0(folderPath, targetName), sep=" ", header = FALSE))
+  if (!single) {
+    expression = sub("^[ab]_", "", sub("_datapoints.txt", "", dataName))
+    data$target = lapply(unlist(targetData), function(x) {
+      if (x > 0) return(expression)
+      else return(as.character(x))
+    })
+  }
   
   # Remove the time column
   return(data[, !names(data) %in% c("X0.0")])
 }
 
-readAllData = function(letter) {
+readAllData = function(letter, single = FALSE) {
   fileNames = choose.files(caption = "Selecciona les dades", multi=TRUE)
   fileNames = grep(paste0(letter, "_.+_datapoints"), fileNames, value = TRUE)
   
   data = data.frame()
   for (i in 1:length(fileNames)) {
-    aux = readData(fileNames[i])
-    if (i == 1) {
-      data = aux
-      print(nrow(data))
-    } else {
-      data = rbind(data, aux)
-      print(nrow(data))
-    }
+    aux = readData(fileNames[i], single)
+    data = rbind(aux, data)
   }
   data$target = as.factor(unlist(data$target))
   return(data)
 }
 
-data = readAllData("a")
