aad6ca424cbbc76e125b18b982f12a8c36220668
Esteve <tarragoesteve@gmail.com>
2017-01-19 01:10:23 +0100

    Finall version

--- R/SVM2_K.R
+++ R/SVM2_K.R
@@ -20,43 +20,39 @@ checkError = function(predicted, real, type) {
 # INICI DE L'EXECUCIO #
 #######################
 
+#######################
+# INICI DE L'EXECUCIO #
+#######################
+
 # Llegir les dades de training
-data.a = readAllData("a", single = FALSE)
-data.b = readAllData("b", single = FALSE)
-data.a.r = reduceData(data.a)
-data.b.r = reduceData(data.b)
-allData = rbind(data.a.r, data.b.r)
-smallData = allData[sample(nrow(allData),1000),]
+allData = reduceData(readAllData( single = FALSE))
+
+# Donat que pel model nomes hi ha dues persones que aporten dades
+# es barregen totes les dades i s'agafen algunes aleatòries per 
+# entrenar el model, després es valida amb la resta de dades
+index.sample = sample(nrow(allData), nrow(allData)/7)
+data.train = allData[index.sample,]
+data.valid = allData[!1:nrow(allData) %in% index.sample,]
 
 # Per comprovar les dades s'usará K-Cross Validation
 
-trc = trainControl(method="cv", number = 10, repeats = 2)
+trc = trainControl(method="cv", number = 4, repeats = 2)
 
 cl = makeCluster(detectCores())
 registerDoParallel(cl)
 
-#The final values used for the model were sigma = 0.5 and C = 10. 
-svm.model = train(target ~ ., data = smallData, method="svmRadial", maxit = 200, trControl = trc,
-                    tuneGrid = expand.grid(C=c(0.1, 1, 10), sigma=c(0.5, 2, 10)))
+#The final values used for the model were sigma = 0.05 and C = 16. 
+svm.model = train(target ~ ., data = data.train, method="svmRadial", maxit = 200, trControl = trc,
+                    tuneGrid = expand.grid(C=c(15, 16, 17), sigma=c(0.01, 0.05, 0.1)))
 stopCluster(cl)
-svmr.pred <- predict(svm.model)
-table(smallData$target, svmr.pred)
-checkError(svmr.pred,smallData$target,"SVM small data")
+svm.pred <- predict(svm.model)
+table(data.train$target, svm.pred)
+checkError(svm.pred,data.train$target,"SVM train")
 
-#### amb totes les dades
-svm.pred <- predict(svm.model, newdata = allData)
-print(table(allData$target,svm.pred))
-checkError(svm.pred,allData$target,"SVM all data")
-
-#### amb les dades del B
-svm.pred <- predict(svm.model, newdata = data.b.r)
-print(table(data.b.r$target,svm.pred))
-checkError(svm.pred,data.b.r$target,"SVM")
-
-#### amb les dades del A
-svm.pred <- predict(svm.model, newdata = data.a.r)
-print(table(data.a.r$target,svm.pred))
-checkError(svm.pred,data.a.r$target,"SVM")
+#### amb totes les de validacio
+svm.pred <- predict(svm.model, newdata = data.valid)
+print(table(data.valid$target,svm.pred))
+checkError(svm.pred,data.valid$target,"SVM test")
 
 
 ####### Linear
@@ -68,24 +64,15 @@ cl = makeCluster(detectCores())
 registerDoParallel(cl)
 
 
-svm.model = train(target ~ ., data = smallData, method="svmLinear", maxit = 200, trControl = trc,
-                  tuneGrid = expand.grid(C=c(0.1, 1, 10)))
+svm.model = train(target ~ ., data = data.train, method="svmLinear", maxit = 200, trControl = trc,
+                  tuneGrid = expand.grid(C=c(8,9,10)))
 stopCluster(cl)
 svm.pred <- predict(svm.model)
-table(smallData$target, svm.pred)
-checkError(svmr.pred,smallData$target,"SVM small data")
+table(data.train$target, svm.pred)
+checkError(svm.pred,data.train$target,"SVM train")
 
-#### amb totes les dades
-svm.pred <- predict(svm.model, newdata = allData)
-print(table(allData$target,svm.pred))
-checkError(svm.pred,allData$target,"SVM all data")
+#### amb totes les de validacio
+svm.pred <- predict(svm.model, newdata = data.valid)
+print(table(data.valid$target,svm.pred))
+checkError(svm.pred,data.valid$target,"SVM test")
 
-#### amb les dades del B
-svm.pred <- predict(svm.model, newdata = data.b.r)
-print(table(data.b.r$target,svm.pred))
-checkError(svm.pred,data.b.r$target,"SVM")
-
-#### amb les dades del A
-svm.pred <- predict(svm.model, newdata = data.a.r)
-print(table(data.a.r$target,svm.pred))
-checkError(svm.pred,data.a.r$target,"SVM")
---
+++ R/SVM2_si_no.R
@@ -0,0 +1,77 @@
+rm(list = ls())
+library(MASS)
+library(kernlab)
+library(caret)
+library(doParallel)
+
+source("./R/readAllData.R")
+source("./R/reduceData.R")
+
+checkError = function(predicted, real, type) {
+  tab = table(predicted, real)
+  print(paste("Error", type))
+  print(tab)
+  
+  error = 100 - (sum(diag(tab)))/length(predicted) * 100
+  print(error)
+}
+
+#######################
+# INICI DE L'EXECUCIO #
+#######################
+
+#######################
+# INICI DE L'EXECUCIO #
+#######################
+
+# Llegir les dades de training
+allData = reduceData(readAllData( single = TRUE))
+
+# Donat que pel model nomes hi ha dues persones que aporten dades
+# es barregen totes les dades i s'agafen algunes aleatòries per 
+# entrenar el model, després es valida amb la resta de dades
+index.sample = sample(nrow(allData), nrow(allData)/7)
+data.train = allData[index.sample,]
+data.valid = allData[!1:nrow(allData) %in% index.sample,]
+
+# Per comprovar les dades s'usará K-Cross Validation
+
+trc = trainControl(method="cv", number = 4, repeats = 2)
+
+cl = makeCluster(detectCores())
+registerDoParallel(cl)
+
+#The final values used for the model were sigma = 0.1 and C = 10.
+svm.model = train(target ~ ., data = data.train, method="svmRadial", maxit = 200, trControl = trc,
+                  tuneGrid = expand.grid(C=c(5,10, 15), sigma=c(0.01, 0.1, 1)))
+stopCluster(cl)
+svm.pred <- predict(svm.model)
+table(data.train$target, svm.pred)
+checkError(svm.pred,data.train$target,"SVM train")
+
+#### amb totes les de validacio
+svm.pred <- predict(svm.model, newdata = data.valid)
+print(table(data.valid$target,svm.pred))
+checkError(svm.pred,data.valid$target,"SVM test")
+
+
+####### Linear
+# Per comprovar les dades s'usará K-Cross Validation
+
+trc = trainControl(method="cv", number = 4, repeats = 2)
+
+cl = makeCluster(detectCores())
+registerDoParallel(cl)
+
+
+svm.model = train(target ~ ., data = data.train, method="svmLinear", maxit = 200, trControl = trc,
+                  tuneGrid = expand.grid(C=c(1, 25, 50, 75, 100)))
+stopCluster(cl)
+svm.pred <- predict(svm.model)
+table(data.train$target, svm.pred)
+checkError(svm.pred,data.train$target,"SVM train")
+
+#### amb totes les de validacio
+svm.pred <- predict(svm.model, newdata = data.valid)
+print(table(data.valid$target,svm.pred))
+checkError(svm.pred,data.valid$target,"SVM test")
