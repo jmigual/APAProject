3030cb89134d5e8f9b23d40ee9ed2cb64c2d8147
Esteve <tarragoesteve@gmail.com>
2017-01-20 15:13:28 +0100

    Versions of random trainning data and validation data

--- R/MLP.R
+++ R/MLP.R
@@ -33,26 +33,35 @@ trainAndError = function(data.train, data.valid, family = quasibinomial, maxit =
   
   # Obtenir el % d'error amb el conjunt de training
   prob.train = predict(glm.res, newdata = data.train, type='response')
-  print(print(str((prob.train))))
-  view(prob.train)
+  #print(print(str((prob.train))))
+  #view(prob.train)
   
   checkError(lapply(prob.train, probValue), data.train[,301], "training")
   
   # Obtenir el % d'error amb el conjunt de validacio
   prob.valid = predict(glm.res, newdata = data.valid)
-  view(prob.valid)
-  checkError(lapply(prob.valid, probValue), data.valid[,301], "validacio")
+  prob.validw = probValue(prob.valid)
+  #view(prob.valid)
+  checkError(prob.validw, data.valid[,301], "validacio")
 }
 
 #######################
 # INICI DE L'EXECUCIO #
 #######################
 # Llegir les dades training
-data.train = readAllData("a", single = FALSE)
+
+allData = readAllData()
+
+index.sample = sample(nrow(allData), nrow(allData)/7)
+data.train = allData[index.sample,]
+data.valid = allData[!1:nrow(allData) %in% index.sample,]
+
+#data.train = readAllData("a", single = FALSE)
 
 # Llegir dades testing
-data.valid = readAllData("b", single = FALSE)
+#data.valid = readAllData("b", single = FALSE)
 
 
 # Regressio utilitzant un model binomial
 trainAndError(data.train, data.valid, "quasibinomial")
+
---
+++ R/QDA_si_no_2.R
@@ -0,0 +1,36 @@
+# Eliminar totes les dades anteriors
+rm(list=ls())
+library(MASS)
+source("./R/readAllData.R")
+
+checkError = function(predicted, real, type) {
+  tab = table(predicted, real)
+  print(paste("Error", type))
+  print(tab)
+  
+  error = 100 - (sum(diag(tab)))/length(predicted) * 100
+  print(error)
+}
+
+#######################
+# INICI DE L'EXECUCIO #
+#######################
+
+allData = readAllData(single=TRUE)
+
+index.sample = sample(nrow(allData), nrow(allData)/2)
+data.train = allData[index.sample,]
+data.valid = allData[!1:nrow(allData) %in% index.sample,]
+
+##Generem el model
+qda.model <- qda (data.train$target ~ ., data.train)
+
+qda.model
+
+# Visualitzacio de l'error de training
+pred.train = predict(qda.model)
+checkError(pred.train$class, data.train$target, "training")
+
+# Visualitzacio de l'error de testing
+pred.valid = predict(qda.model, newdata = data.valid)
+checkError(pred.valid$class, data.valid$target, "validacio")
